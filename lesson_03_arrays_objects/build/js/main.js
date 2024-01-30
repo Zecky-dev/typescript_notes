b823f228d5ed0e629e4f00256cd91df87979ca04
Zecky-Dev <l2021012041@ogr.sdu.edu.tr>
2024-01-30 23:56:41 +0300

    arrays, union arrays, tuples, objects, types, interfaces, enums

---
+++ lesson_03_arrays_objects/build/js/main.js
@@ -0,0 +1,65 @@
+"use strict";
+// arrays
+// string[]
+let stringArr = ['one', 'hey', 'zek'];
+// (string | number)[]
+let guitars = ['Strat', 'Les Paul', 5150];
+// (string | number | boolean | null)[]
+let mixedData = ['EVH', 1984, true, null];
+// stringArr[0] = 5 -> ts shows error because all elements inside stringArr must be a string
+//stringArr.push(50)
+guitars.push(70);
+guitars.push('New Guitar Type');
+guitars[0] = 50;
+// order of data types is not important, for example first element in guitars array is a string but we can assign a number to this element because it's union type array
+// if an union type covers other union type then big union would be use reference as litle union type
+guitars = stringArr;
+mixedData = guitars;
+// if we don't pass any value to an array typescript infers it as any type
+let test = [];
+let bands = [];
+bands.push("Test Band");
+// bands.push(false) --> It's not valid usage because we can only pass string type data to that array
+// Tuple
+// We can define custom order data type arrays
+let myTuple = ["Hello", 5, false];
+let mixed = ['John', 1, false];
+mixed = myTuple;
+// Objects
+let myObject;
+myObject = []; // arrays in js -> object
+console.log(typeof (myObject)); // typeof(myObject) -> object
+myObject = {};
+myObject = bands;
+const exampleObj = {
+    prop1: 'Zekican',
+    prop2: true
+};
+let evh = {
+    name: "Zekican",
+    active: false,
+    albums: ["First Album", 2001]
+};
+let jp = {
+    name: "Taner",
+    albums: ["Hello world", 1984]
+};
+const greetGuitarist = (guitarist) => {
+    console.log(guitarist.name, guitarist.active, guitarist.albums);
+};
+greetGuitarist(evh);
+let car = {
+    brand: "Toyota",
+    model: "Supra",
+    type: "Sport"
+};
+// Enums
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 0] = "U";
+    Grade[Grade["D"] = 1] = "D";
+    Grade[Grade["C"] = 2] = "C";
+    Grade[Grade["B"] = 3] = "B";
+    Grade[Grade["A"] = 4] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U);
---
+++ lesson_03_arrays_objects/src/main.ts
@@ -0,0 +1,115 @@
+// arrays
+
+// string[]
+let stringArr = ['one','hey','zek']
+
+// (string | number)[]
+let guitars = ['Strat','Les Paul', 5150]
+
+// (string | number | boolean | null)[]
+let mixedData = ['EVH', 1984, true,null]
+
+// stringArr[0] = 5 -> ts shows error because all elements inside stringArr must be a string
+//stringArr.push(50)
+
+guitars.push(70) 
+guitars.push('New Guitar Type')
+guitars[0] = 50
+// order of data types is not important, for example first element in guitars array is a string but we can assign a number to this element because it's union type array
+
+
+// if an union type covers other union type then big union would be use reference as litle union type
+guitars = stringArr
+mixedData = guitars
+
+// if we don't pass any value to an array typescript infers it as any type
+let test = []
+
+let bands: string[] = []
+bands.push("Test Band")
+// bands.push(false) --> It's not valid usage because we can only pass string type data to that array
+
+// Tuple
+// We can define custom order data type arrays
+let myTuple: [string,number,boolean] = ["Hello",5,false]
+let mixed = ['John',1,false]
+mixed = myTuple
+
+// Objects
+let myObject: object
+myObject = [] // arrays in js -> object
+console.log(typeof(myObject)) // typeof(myObject) -> object
+
+myObject = {}
+myObject = bands
+
+const exampleObj = {
+    prop1: 'Zekican',
+    prop2: true
+}
+
+// exampleObj.prop1 = 5 -> prop1 type is string so we can't assign a number to that property
+
+
+// type
+type Gutitarist = {
+    name: string,
+    active?: boolean, // we can make a field optional
+    albums: (string | number)[]
+}
+
+let evh: Gutitarist = {
+    name: "Zekican",
+    active: false,
+    albums: ["First Album",2001]
+}
+
+let jp: Gutitarist = {
+    name: "Taner",
+    albums: ["Hello world",1984]
+}
+
+const greetGuitarist = (guitarist:Gutitarist) => {
+    console.log(guitarist.name,guitarist.active,guitarist.albums)
+}
+
+greetGuitarist(evh)
+
+// interface
+interface Car {
+    brand: string,
+    model: string,
+    type: string,
+    doorCount?: number
+}
+
+let car : Car = {
+    brand: "Toyota",
+    model: "Supra",
+    type: "Sport"
+}
+
+// Enums
+enum Grade {
+    U,
+    D,
+    C,
+    B,
+    A
+}
+
+console.log(Grade.U)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
